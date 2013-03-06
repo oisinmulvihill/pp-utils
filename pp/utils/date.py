2077a1521af00ead844da87d1fae8e32946e6331
Edward Easton <edward.easton@pythonpro.co.uk>
2013-03-06 22:05:10 +0000

    Some sketching out of timereferences

--- pp/utils/date.py
+++ pp/utils/date.py
@@ -13,7 +13,38 @@ OPEN_CLOSED = 2
 OPEN_OPEN = 3
 
 
-class DateRange(object):
+class TimeReference(object):
+    """ Represents a piece of data referring to date or time
+        Eg:
+           A date range:   (start='2013-08-14',
+                            end='2013-08-15', interval=CLOSED_CLOSED)
+           A point in time:  '2013-08-14 18:00'
+           A fuzzy match:  'Tuesday afternoon'
+           A range of dates: 'Weekly on Tuesday at 18:00'
+    """
+    def __init__(self):
+        pass
+
+
+class PointInTime(TimeReference):
+    def __init__(self, point):
+        self.point = dateutil.parser.parse(point)
+
+
+class FuzzyTimeReference(TimeReference):
+    pass
+
+
+class RepeatingTimeReference(TimeReference):
+    def __init__(self, start_at, duration, period,
+                 stop_at=None):
+        self.start_at = start_at
+        self.stop_at = stop_at
+        self.duration = duration
+        self.period = period
+
+
+class DateRange(TimeReference):
     """
     Date range representation.
     """
